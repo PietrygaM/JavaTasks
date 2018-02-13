417e4209d24da3ac67c68ee9826e6ba64a771beb
PietrygaM <pietryga.marcin@gmail.com>
2018-02-13 16:08:38 +0100

    update

--- src/Task079.java
+++ src/Task079.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Task79 {
+public class Task079 {
 	public static void main(String[] args) {
 
 		Map<String, Task079A> map = getMap(initialize());
